0e74583c62965aa09e5edb4e779c0c5dcf1a7fa8
Nika Elashvili <nika.elashvili@gmail.com>
2020-10-21 12:07:00 -0700

    Color

--- Sources/DotsScaleActivityIndicator/DotsScaleActivityIndicator.swift
+++ Sources/DotsScaleActivityIndicator/DotsScaleActivityIndicator.swift
@@ -12,30 +12,35 @@ import SwiftUI
 public struct DotsScaleActivityIndicator: View {
     @State private var shouldAnimate = false
     let diameter: CGFloat
+    let color: Color
 
-    public init(diametre: CGFloat? = 8) {
+    public init(diametre: CGFloat? = 8, color: Color? = .blue) {
         self.diameter = diametre ?? 8
+        self.color = color ?? .blue
     }
 
     public var body: some View {
         HStack {
-            Circle()
-                .fill(Color.blue)
-                .frame(width: diameter, height: diameter)
-                .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                .animation(Animation.easeInOut(duration: 0.5).repeatForever())
-            Circle()
-                .fill(Color.blue)
-                .frame(width: diameter, height: diameter)
-                .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.3))
-            Circle()
-                .fill(Color.blue)
-                .frame(width: diameter, height: diameter)
-                .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.6))
+            if shouldAnimate {
+                Circle()
+                    .fill(color)
+                    .frame(width: diameter, height: diameter)
+                    .scaleEffect(shouldAnimate ? 1.0 : 0.5)
+                    .animation(Animation.easeInOut(duration: 0.5).repeatForever())
+                Circle()
+                    .fill(color)
+                    .frame(width: diameter, height: diameter)
+                    .scaleEffect(shouldAnimate ? 1.0 : 0.5)
+                    .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.3))
+                Circle()
+                    .fill(color)
+                    .frame(width: diameter, height: diameter)
+                    .scaleEffect(shouldAnimate ? 1.0 : 0.5)
+                    .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.6))
+            }
         }
         .frame(height: 12)
+        .padding()
         .onAppear {
             self.shouldAnimate = true
         }
@@ -47,7 +52,7 @@ public struct DotsScaleActivityIndicator: View {
 }
 
 //public struct DotsScaleActivityIndicator_Previews: PreviewProvider {
-//    @available(OSX 10.15.0, *)
+//    @available(iOS 13.0.0, *)
 //    public static var previews: some View {
 //            DotsScaleActivityIndicator()
 //    }
